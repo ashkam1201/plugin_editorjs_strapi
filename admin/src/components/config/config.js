20f2bfca6f714c6468ce7d2e7e15891b007d0b9e
Tim Izzo <tim@octree.ch>
2021-05-21 16:20:58 +0200

    🐛 Fix path and exports

--- admin/src/components/config/config.js
+++ admin/src/components/config/config.js
@@ -8,74 +8,78 @@ import List from '@editorjs/list';
 import Checklist from '@editorjs/checklist';
 import ImageTool from '@editorjs/image';
 
-
 const tools = {
-    header: {
-      class: Header,
-      config: {
-        defaultLevel: 1
-      }
+  header: {
+    class: Header,
+    config: {
+      defaultLevel: 1,
     },
-    quote: {
-      class: Quote,
-      inlineToolbar: true,
-    },
-    embed: {
-      class: Embed,
-      config: {
-        services: {
-          youtube: true,
-        },
+  },
+  quote: {
+    class: Quote,
+    inlineToolbar: true,
+  },
+  embed: {
+    class: Embed,
+    config: {
+      services: {
+        youtube: true,
       },
     },
-    marker: Marker,
-    table: {
-      class: Table,
-      inlineToolbar: true,
-    },
-    list: {
-      class: List,
-      inlineToolbar: true,
-    },
-    delimiter: Delimiter,
-    checklist: {
-      class: Checklist,
-      inlineToolbar: true,
-    },
-    image: {
-      class: ImageTool,
-      config: {
-        endpoints: {
-          byFile: `${strapi.backendURL}/upload`, // Your backend file uploader endpoint
-          byUrl: `${strapi.backendURL}/upload`, // Your endpoint that provides uploading by Url
-        },
-        additionalRequestHeaders: {
-          authorization: `Bearer ${auth.getToken()}`,
-        },
-        uploader: {
-          /**
-           * Upload file to the server and return an uploaded image data
-           * @param {File} file - file selected from the device or pasted by drag-n-drop
-           * @return {Promise.<{success, file: {url}}>}
-           */
-          uploadByFile(file) {
-            // your own uploading logic here
-            const formData = new FormData();
-            formData.append('files', file);
-            const headers = {};
+  },
+  marker: Marker,
+  table: {
+    class: Table,
+    inlineToolbar: true,
+  },
+  list: {
+    class: List,
+    inlineToolbar: true,
+  },
+  delimiter: Delimiter,
+  checklist: {
+    class: Checklist,
+    inlineToolbar: true,
+  },
+  // image: {
+  //   class: ImageTool,
+  //   config: {
+  //     endpoints: {
+  //       byFile: `${strapi.backendURL}/upload`, // Your backend file uploader endpoint
+  //       byUrl: `${strapi.backendURL}/upload`, // Your endpoint that provides uploading by Url
+  //     },
+  //     additionalRequestHeaders: {
+  //       authorization: `Bearer ${auth.getToken()}`,
+  //     },
+  //     uploader: {
+  //       /**
+  //        * Upload file to the server and return an uploaded image data
+  //        * @param {File} file - file selected from the device or pasted by drag-n-drop
+  //        * @return {Promise.<{success, file: {url}}>}
+  //        */
+  //       uploadByFile(file) {
+  //         // your own uploading logic here
+  //         const formData = new FormData();
+  //         formData.append("files", file);
+  //         const headers = {};
 
-            return request('/upload', { method: 'POST', headers, body: formData }, false, false).then(resp => {
-              return {
-                success: 1,
-                file: {
-                  url: `${strapi.backendURL}${resp[0].url}`,
-                },
-              };
-            });
-          },
-        },
-      },
-    },
-  };
+  //         return request(
+  //           "/upload",
+  //           { method: "POST", headers, body: formData },
+  //           false,
+  //           false
+  //         ).then((resp) => {
+  //           return {
+  //             success: 1,
+  //             file: {
+  //               url: `${strapi.backendURL}${resp[0].url}`,
+  //             },
+  //           };
+  //         });
+  //       },
+  //     },
+  //   },
+  // },
+};
 
-  export {tools}
+export { tools };
--- admin/src/components/editorjs/index.js
+++ admin/src/components/editorjs/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 // The Editor.js react component src : https://github.com/natterstefan/react-editor-js
 import EditorJs from '@natterstefan/react-editor-js';
-import tools from './config.js'
+import tools from '../config/config.js';
 
 const Wrapper = styled.div`
   .editorjs__main {
@@ -20,38 +20,37 @@ const Editor = () => {
   const onSave = async () => {
     // https://editorjs.io/saving-data
     try {
-      const outputData = await editor.save()
-      console.log('Article data: ', outputData)
+      const outputData = await editor.save();
+      console.log('Article data: ', outputData);
     } catch (e) {
-      console.log('Saving failed: ', e)
+      console.log('Saving failed: ', e);
     }
-  }
+  };
 
   const onReady = () => {
     // https://editorjs.io/configuration#editor-modifications-callback
-    console.log('Editor.js is ready to work!')
-  }
+    console.log('Editor.js is ready to work!');
+  };
 
   const onChange = () => {
     // https://editorjs.io/configuration#editor-modifications-callback
-    console.log("Now I know that Editor's content changed!")
-  }
-
+    console.log("Now I know that Editor's content changed!");
+  };
 
   return (
     <Wrapper>
-
       <button onClick={onSave}>Save</button>
 
       <EditorJs
         onReady={onReady}
         onChange={onChange}
         tools={tools}
-        editorInstance={editorInstance => {
+        editorInstance={(editorInstance) => {
           editor = editorInstance;
         }}
       />
-          </Wrapper>
-          );
+    </Wrapper>
+  );
+};
 
-      export Editor
+export default Editor;
